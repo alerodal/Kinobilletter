1aec39cd4146e79085f2d6daf87fcef54b65182a
Aleksander Rødal <alrod3388@oslomet.no>
2024-05-06 15:54:57 +0200

    Add files via upload

---
+++ Oblig3/target/classes/templates/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO Kino (Film, Antall, Fornavn, Etternavn, Telefonnr, Epost) VALUES ('Hunger Games 2', 3, 'Per', 'Hansen', '99163211', 'phansen@gmail.com');
+INSERT INTO Kino (Film, Antall, Fornavn, Etternavn, Telefonnr, Epost) VALUES ('JackAss 4', 2, 'Nils', 'Evensen', '40822134', 'enils@gmail.com');
