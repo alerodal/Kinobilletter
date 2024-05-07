f3bc96d03cf0f2d89938602308b67e4fc8eae604
Aleksander Rødal <alrod3388@oslomet.no>
2024-05-07 14:43:57 +0200

    Delete Oblig3 directory

--- Oblig3/src/main/resources/templates/mdata.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO Biler(merke, type) VALUES('Volkswagen', 'Polo');
-INSERT INTO Biler(merke, type) VALUES('Toyota', 'Corolla');
-INSERT INTO Biler(merke, type) VALUES('Volkswagen', 'Beetle');
-INSERT INTO Biler(merke, type) VALUES('Suzuki', 'Swift');
-INSERT INTO Biler(merke, type) VALUES('Suzuki', 'Cultus');
-INSERT INTO Biler(merke, type) VALUES('Suzuki', 'Alto');
--- Oblig3/target/classes/templates/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO Kino (Film, Antall, Fornavn, Etternavn, Telefonnr, Epost) VALUES ('Hunger Games 2', 3, 'Per', 'Hansen', '99163211', 'phansen@gmail.com');
-INSERT INTO Kino (Film, Antall, Fornavn, Etternavn, Telefonnr, Epost) VALUES ('JackAss 4', 2, 'Nils', 'Evensen', '40822134', 'enils@gmail.com');
